0b345056c70258c08349d1bc1cea36a9cdde3942
Kociak Michał <Kociak.Michal@zabka.pl>
2020-07-24 08:11:25 +0200

    add brama

---
+++ brama.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+  setInterval(() => {
+    const refreshBtns = document.querySelectorAll("button.refreshNow");
+    // Wykonanie clicka na kaĹźdym pobranym przycisku
+    for (const refreshBtn of refreshBtns) {
+      refreshBtn.click();
+    }
+    // Co 5 minut
+  }, 300000);
+};
