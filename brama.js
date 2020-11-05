6cdb5ce40a628592ae428e1b67fe5abb83a5cb43
gdanskit <68724537+gdanskit@users.noreply.github.com>
2020-11-05 05:24:55 +0100

    Update brama.js

--- brama.js
+++ brama.js
@@ -1,8 +1,9 @@
 
   setInterval(() => {
     const refreshBtns = document.querySelectorAll('button.quick-action-button.ng-star-inserted');
+    setTimeout(() => {
     refreshBtns[0].click();
     refreshBtns[2].click();
     refreshBtns[4].click();
-    refreshBtns[6].click();
+    refreshBtns[6].click();}, 5000)
   }, 300000);
