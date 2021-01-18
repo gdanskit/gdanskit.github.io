d75a2e5265076802734e174744578f303755d11c
gdanskit <68724537+gdanskit@users.noreply.github.com>
2021-01-19 00:42:07 +0100

    Update test123.js

--- test123.js
+++ test123.js
@@ -2,21 +2,10 @@ const pickReports = "https://app.powerbi.com/groups/me/reports/88ade1c7-00b6-444
 const foodSection1 = "ReportSectione895c1142160cc56ce23";
 const foodSection2 = "ReportSection13788903743ec64ca747";
 const akcSection = "ReportSection7817c8af9e009298779b";
-const chSection = "ReportSectionc2c587df6985409de992";
-const mrSection = "ReportSection1d4a751bb7383daab4d1";
 const fsSection = "ReportSection84c9ccb8aff1a90475c5";
-const pickCovidInfo_PL = "ReportSection7441e49f704a0b24e85a";
-const pickCovidInfo_UA = "ReportSection89185ba5c51b6b8b80ea";
 
 const shipmentsReports = "https://app.powerbi.com/groups/me/reports/76a3d701-9159-41b1-b175-3574f805cd41/";
 const shipmentsMG = "ReportSection3a36623f6ea27584e358";
-const shipmentCovidInfo_PL = "ReportSection6fd1415e2068042d3b28";
-const shipmentCovidInfo_UA = "ReportSection6392120b100130bec981";
-
-const shippingsReports = "https://app.powerbi.com/groups/me/reports/76a3d701-9159-41b1-b175-3574f805cd41/";
-const embarkationReport = "ReportSection3a36623f6ea27584e358";
-const shippingsCovidInfo_PL = "ReportSection15702083ade0c20401dd";
-const shippingsCovidInfo_UA = "ReportSectione42e65caa621347242e2";
 
 const checkTime = () => {
   const currentHour = new Date().getHours();
@@ -27,25 +16,11 @@ setInterval(() => {
   if (checkTime() >= 6 && checkTime() <= 13) {
     if (config.shipmentsMG) {
       if (
-        window.location.href === shipmentsReports + shipmentsMG ||
-        window.location.href.includes(shipmentCovidInfo_PL) ||
-        window.location.href.includes(shipmentCovidInfo_UA)
+        window.location.href === shipmentsReports + shipmentsMG 
       ) {
         return;
       } else {
         window.location = shipmentsReports + shipmentsMG;
-      }
-    }
-
-    if (config.shipmentsCH) {
-      if (
-        window.location.href.includes(shipmentsCH) ||
-        window.location.href.includes(shipmentCovidInfo_PL) ||
-        window.location.href.includes(shipmentCovidInfo_UA)
-      ) {
-        return;
-      } else {
-        window.location = shipmentsReports + shipmentsCH;
       }
     }
   }
@@ -54,9 +29,7 @@ setInterval(() => {
     if (config.food) {
       if (
         window.location.href.includes(foodSection1) ||
-        window.location.href.includes(foodSection2) ||
-        window.location.href.includes(pickCovidInfo_PL) ||
-        window.location.href.includes(pickCovidInfo_UA)
+        window.location.href.includes(foodSection2) 
       ) {
         return;
       } else {
@@ -66,9 +39,7 @@ setInterval(() => {
 
     if (config.fs) {
       if (
-        window.location.href.includes(fsSection) ||
-        window.location.href.includes(pickCovidInfo_PL) ||
-        window.location.href.includes(pickCovidInfo_UA)
+        window.location.href.includes(fsSection) 
       ) {
         return;
       } else {
@@ -78,47 +49,11 @@ setInterval(() => {
 
     if (config.akc) {
       if (
-        window.location.href.includes(akcSection) ||
-        window.location.href.includes(pickCovidInfo_PL) ||
-        window.location.href.includes(pickCovidInfo_UA)
+        window.location.href.includes(akcSection) 
       ) {
         return;
       } else {
         window.location = pickReports + akcSection;
-      }
-    }
-
-    if (config.ch) {
-      if (
-        window.location.href.includes(chSection) ||
-        window.location.href.includes(pickCovidInfo_PL) ||
-        window.location.href.includes(pickCovidInfo_UA)
-      ) {
-        return;
-      } else {
-        window.location = pickReports + chSection;
-      }
-    }
-    if (config.mr) {
-      if (
-        window.location.href.includes(mrSection) ||
-        window.location.href.includes(pickCovidInfo_PL) ||
-        window.location.href.includes(pickCovidInfo_UA)
-      ) {
-        return;
-      } else {
-        window.location = pickReports + mrSection;
-      }
-    }
-    if (config.shippings) {
-      if (
-        window.location.href.includes(embarkationReport) ||
-        window.location.href.includes(shippingsCovidInfo_PL) ||
-        window.location.href.includes(shippingsCovidInfo_UA)
-      ) {
-        return;
-      } else {
-        window.location = shippingsReports + embarkationReport;
       }
     }
   }
@@ -130,7 +65,7 @@ if (
 ) {
   setTimeout(() => {
     // Pobranie wszystkich zakładek
-      const tab = document.querySelectorAll(".itemRow.pbi-focus-outline")
+    const tab = document.querySelectorAll("div.itemRow.pbi-focus-outline");
     setInterval(() => {
       // Kliknięcie pierwszej zakładki
       tab[0].click();
@@ -139,143 +74,40 @@ if (
         // Kliknięcie drugiej zakładki
         tab[1].click();
         // Co 30 sekund
-      }, 30000);
+      }, 45000);
       // Cały cykl się powtarza co 60 sekund
-    }, 60000);
+    }, 90000);
     // Po 5 sekundach od wejścia pierwszy raz na stronę
   }, 5000);
 } else if (
-  window.location.href === shipmentsReports + shipmentsMG ||
-  window.location.href.includes(shipmentCovidInfo_PL) ||
-  window.location.href.includes(shipmentCovidInfo_UA)
+  window.location.href === shipmentsReports + shipmentsMG 
 ) {
   setTimeout(() => {
-    // Pobranie wszystkich zakładek
-    const tab = document.querySelectorAll("div[role=tab]");
     setInterval(() => {
-      // Kliknięcie pierwszej zakładki
-      tab[0].click();
       document.querySelector("button#reportAppBarRefreshBtn").click();
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[7].click();
-        // Co 30 sekund
-      }, 30000);
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[8].click();
-        // Co 30 sekund
-      }, 60000);
       // Cały cykl się powtarza co 60 sekund
     }, 90000);
     // Po 5 sekundach od wejścia pierwszy raz na stronę
   }, 5000);
 } else if (window.location.href.includes(fsSection)) {
   setTimeout(() => {
-    const tab = document.querySelectorAll("div[role=tab]");
+    const tab = document.querySelectorAll("div.itemRow.pbi-focus-outline");
 
     setInterval(() => {
-      tab[5].click();
+      tab[3].click();
       document.querySelector("button#reportAppBarRefreshBtn").click();
 
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[7].click();
-        // Co 30 sekund
-      }, 30000);
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[8].click();
-        // Co 30 sekund
-      }, 60000);
       // Cały cykl się powtarza co 90 sekund
     }, 90000);
     // Po 5 sekundach od wejścia pierwszy raz na stronę
   }, 5000);
 } else if (window.location.href.includes(akcSection)) {
   setTimeout(() => {
-    const tab = document.querySelectorAll("div[role=tab]");
+    const tab = document.querySelectorAll("div.itemRow.pbi-focus-outline");
     setInterval(() => {
       tab[2].click();
       document.querySelector("button#reportAppBarRefreshBtn").click();
 
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[7].click();
-        // Co 30 sekund
-      }, 30000);
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[8].click();
-        // Co 30 sekund
-      }, 60000);
-      // Cały cykl się powtarza co 90 sekund
-    }, 90000);
-    // Po 5 sekundach od wejścia pierwszy raz na stronę
-  }, 15000);
-
-  //Chłodnia
-} else if (window.location.href.includes(chSection)) {
-  setTimeout(() => {
-    const tab = document.querySelectorAll("div[role=tab]");
-    setInterval(() => {
-      tab[3].click();
-      document.querySelector("button#reportAppBarRefreshBtn").click();
-
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[7].click();
-        // Co 30 sekund
-      }, 30000);
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[8].click();
-        // Co 30 sekund
-      }, 60000);
-      // Cały cykl się powtarza co 90 sekund
-    }, 90000);
-    // Po 5 sekundach od wejścia pierwszy raz na stronę
-  }, 5000);
-
-  //Mroźnia
-} else if (window.location.href.includes(mrSection)) {
-  setTimeout(() => {
-    const tab = document.querySelectorAll("div[role=tab]");
-    setInterval(() => {
-      tab[4].click();
-      document.querySelector("button#reportAppBarRefreshBtn").click();
-
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[7].click();
-        // Co 30 sekund
-      }, 30000);
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[8].click();
-        // Co 30 sekund
-      }, 60000);
-      // Cały cykl się powtarza co 90 sekund
-    }, 90000);
-    // Po 5 sekundach od wejścia pierwszy raz na stronę
-  }, 5000);
-} else if (window.location.href.includes(embarkationReport)) {
-  setTimeout(() => {
-    const tab = document.querySelectorAll("div[role=tab]");
-    setInterval(() => {
-      tab[0].click();
-      document.querySelector("button#reportAppBarRefreshBtn").click();
-
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[2].click();
-        // Co 30 sekund
-      }, 30000);
-      setTimeout(() => {
-        // Kliknięcie drugiej zakładki
-        tab[3].click();
-        // Co 30 sekund
-      }, 60000);
       // Cały cykl się powtarza co 90 sekund
     }, 90000);
     // Po 5 sekundach od wejścia pierwszy raz na stronę
