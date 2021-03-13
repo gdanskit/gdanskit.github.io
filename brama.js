2d1e55fcf8b58217e50dd2aca20f47f52f53c753
gdanskit <68724537+gdanskit@users.noreply.github.com>
2021-03-13 09:06:37 +0100

    Update brama.js

--- brama.js
+++ brama.js
@@ -1,7 +1,7 @@
 setInterval(() => {
     const refreshBtns = document.querySelectorAll('button.actionPanelButton.refreshButton.mat-focus-indicator.mat-button.mat-button-base.ng-star-inserted')
     for (let i = 0; i <= 4; i++) {
-        if(i !== 1) {
+        if(i !== 1 || i !== 2) {
         refreshBtns[i].click();
         }
         
